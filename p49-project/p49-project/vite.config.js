c93ec86098b0b2ae63644d69ac689083b7cbf148
wshereni <wilfredjshereni@gmail.com>
2025-11-05 23:31:04 -0600

    updated build

--- p49-project/p49-project/vite.config.js
+++ p49-project/p49-project/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-plugins: [react()],
-base: '/p49/',
-build: {
-outDir: 'docs'
-}
+  base: '/p49/',
+  build: { outDir: '../docs' },
+  plugins: [react()]
 })
