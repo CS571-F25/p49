53b2a003baa0fae775c70946a71caeb80cc0cf18
Usman Mohammed <umohammed2@wisc.edu>
2025-11-19 22:18:45 -0600

    Spelling correction

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ export default function Home(props){
     return <div>
         <h1>Campus Cravings</h1>
         <p className="lead">
-            Discover the best bites around UW-Madison's campus! Browse student favorite, hidden gems, and late-night bites.
+            Discover the best bites around UW-Madison's campus! Browse student favorites, hidden gems, and late-night bites.
         </p>
     </div>
 }
